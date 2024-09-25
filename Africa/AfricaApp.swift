cf9caeec7e8f04f440bc82315b0f351e9f685bd0
HajdukSanchez <hajduksanchez.dev@gmail.com>
2024-09-24 19:25:11 -0500

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jozek Andrzej Hajduk Sanchez on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
